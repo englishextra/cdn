105dbc6d492885b116792fdeac281499879266cd
englishextra <englishextra@yandex.ru>
2016-12-28 01:02:39 +0300

    changed functions to var

--- bala/0.1.9/js/bala.fixed.js
+++ bala/0.1.9/js/bala.fixed.js
@@ -6,10 +6,9 @@
  * a=BALA("sometag/#someid/.someclass"[,someParent]);
  * a=BALA.one("sometag/#someid/.someclass"[,someParent]);
  * global $ becomes var g
- * exposed as window property
- * with the name BALA
  * renamed function $ to g
+ * added window object existence check
  * source: github.com/finom/bala/blob/master/bala.js
  * passes jshint
  */
-;(function(){var g=(function(document,s_addEventListener,s_querySelectorAll){function g(s,context,bala){bala=Object.create(g.fn);if(s){bala.push.apply(bala,s[s_addEventListener]?[s]:""+s===s?/</.test(s)?((context=document.createElement(context||s_addEventListener)).innerHTML=s,context.children):context?((context=g(context)[0])?context[s_querySelectorAll](s):bala):document[s_querySelectorAll](s):typeof s=='function'?document.readyState[7]?s():document[s_addEventListener]('DOMContentLoaded',s):s);}return bala;}g.fn=[];g.one=function(s,context){return g(s,context)[0]||null;};return g;})(document,'addEventListener','querySelectorAll');window.BALA=g;}());
+var BALA=(function(){if("undefined"==typeof window||!("document"in window)){return console.log("window is undefined or document is not in window"),!1;}var g=(function(document,s_addEventListener,s_querySelectorAll){function g(s,context,bala){bala=Object.create(g.fn);if(s){bala.push.apply(bala,s[s_addEventListener]?[s]:""+s===s?/</.test(s)?((context=document.createElement(context||s_addEventListener)).innerHTML=s,context.children):context?((context=g(context)[0])?context[s_querySelectorAll](s):bala):document[s_querySelectorAll](s):typeof s=='function'?document.readyState[7]?s():document[s_addEventListener]('DOMContentLoaded',s):s);}return bala;}g.fn=[];g.one=function(s,context){return g(s,context)[0]||null;};return g;})(document,'addEventListener','querySelectorAll');return g;}());
