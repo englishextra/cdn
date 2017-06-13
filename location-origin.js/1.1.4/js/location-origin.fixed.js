74f486ad031f62ae404882f6bd99f88b98a91424
englishextra <englishextra@yandex.ru>
2017-06-14 01:19:08 +0300

    tidy up

--- location-origin.js/1.1.4/js/location-origin.fixed.js
+++
@@ -1,7 +0,0 @@
-/*!
- * modified Polyfill for window.location.origin
- * @see {@link https://github.com/shinnn/location-origin.js}
- * compiled with closure-compiler.appspot.com/home
- * passes jshint
- */
-(function(){var a,b;a=window.location;if(!a.origin){b=a.protocol+"//"+a.hostname+(a.port?":"+a.port:"");try{Object.defineProperty(a,"origin",{value:b,enumerable:!0});}catch(c){a.origin=b;}}}).call(this);
