d1b2524db1b8788878573847ec2e881b49d28d0c
englishextra <englishextra@yandex.ru>
2017-11-22 18:24:43 +0300

    updates

--- ToProgress/0.1.1/js/ToProgress.fixed.js
+++ ToProgress/0.1.1/js/ToProgress.fixed.js
@@ -50,7 +50,8 @@
 					id: "top-progress-bar",
 					color: "#F44336",
 					height: "2px",
-					duration: 0.2
+					duration: 0.2,
+					zIndex: "auto"
 				};
 				if (opt && typeof opt === "object") {
 					for (var key in opt) {
