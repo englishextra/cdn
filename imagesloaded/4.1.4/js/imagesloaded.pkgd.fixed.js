1cc8787b9267ebbb67447e3c1d59090c46cec080
englishextra <englishextra@yandex.ru>
2018-12-01 12:31:01 +0300

    updates

--- imagesloaded/4.1.4/js/imagesloaded.pkgd.fixed.js
+++ imagesloaded/4.1.4/js/imagesloaded.pkgd.fixed.js
@@ -1,16 +1,136 @@
 /*!
- * @see {@link https://github.com/desandro/imagesloaded/blob/master/imagesloaded.js}
- * passes jshint
+ * imagesLoaded PACKAGED v4.1.4
+ * JavaScript is all like "You images are done yet or what?"
+ * MIT License
  */
+
+/**
+ * EvEmitter v1.1.0
+ * Lil' event emitter
+ * MIT License
+ */
+
+/* jshint unused: true, undef: true, strict: true */
+
+(function (global, factory) {
+	// universal module definition
+	/* jshint strict: false */
+	/* globals define, module, window */
+	if (typeof define == 'function' && define.amd) {
+		// AMD - RequireJS
+		define('ev-emitter/ev-emitter', factory);
+	} else if (typeof module == 'object' && module.exports) {
+		// CommonJS - Browserify, Webpack
+		module.exports = factory();
+	} else {
+		// Browser globals
+		global.EvEmitter = factory();
+	}
+
+}
+	(typeof window != 'undefined' ? window : this, function () {
+
+		"use strict";
+
+		function EvEmitter() {}
+
+		var proto = EvEmitter.prototype;
+
+		proto.on = function (eventName, listener) {
+			if (!eventName || !listener) {
+				return;
+			}
+			// set events hash
+			var events = this._events = this._events || {};
+			// set listeners array
+			var listeners = events[eventName] = events[eventName] || [];
+			// only add once
+			if (listeners.indexOf(listener) == -1) {
+				listeners.push(listener);
+			}
+
+			return this;
+		};
+
+		proto.once = function (eventName, listener) {
+			if (!eventName || !listener) {
+				return;
+			}
+			// add event
+			this.on(eventName, listener);
+			// set once flag
+			// set onceEvents hash
+			var onceEvents = this._onceEvents = this._onceEvents || {};
+			// set onceListeners object
+			var onceListeners = onceEvents[eventName] = onceEvents[eventName] || {};
+			// set flag
+			onceListeners[listener] = true;
+
+			return this;
+		};
+
+		proto.off = function (eventName, listener) {
+			var listeners = this._events && this._events[eventName];
+			if (!listeners || !listeners.length) {
+				return;
+			}
+			var index = listeners.indexOf(listener);
+			if (index != -1) {
+				listeners.splice(index, 1);
+			}
+
+			return this;
+		};
+
+		proto.emitEvent = function (eventName, args) {
+			var listeners = this._events && this._events[eventName];
+			if (!listeners || !listeners.length) {
+				return;
+			}
+			// copy over to avoid interference if .off() in listener
+			listeners = listeners.slice(0);
+			args = args || [];
+			// once stuff
+			var onceListeners = this._onceEvents && this._onceEvents[eventName];
+
+			for (var i = 0; i < listeners.length; i++) {
+				var listener = listeners[i];
+				var isOnce = onceListeners && onceListeners[listener];
+				if (isOnce) {
+					// remove listener
+					// remove before trigger to prevent recursion
+					this.off(eventName, listener);
+					// unset once flag
+					delete onceListeners[listener];
+				}
+				// trigger listener
+				listener.apply(this, args);
+			}
+
+			return this;
+		};
+
+		proto.allOff = function () {
+			delete this._events;
+			delete this._onceEvents;
+		};
+
+		return EvEmitter;
+
+	}));
+
 /*!
  * imagesLoaded v4.1.4
  * JavaScript is all like "You images are done yet or what?"
  * MIT License
  */
+
 (function (window, factory) {
 	'use strict';
 	// universal module definition
+
 	/*global define: false, module: false, require: false */
+
 	if (typeof define == 'function' && define.amd) {
 		// AMD
 		define([
@@ -29,13 +149,20 @@
 				window,
 				window.EvEmitter);
 	}
+
 })(typeof window !== 'undefined' ? window : this,
+
 	// --------------------------  factory -------------------------- //
+
 	function factory(window, EvEmitter) {
-	'use strict';
+
+	"use strict";
+
 	var $ = window.jQuery;
 	var console = window.console;
+
 	// -------------------------- helpers -------------------------- //
+
 	// extend objects
 	function extend(a, b) {
 		for (var prop in b) {
@@ -45,22 +172,28 @@
 		}
 		return a;
 	}
+
 	var arraySlice = Array.prototype.slice;
+
 	// turn element or nodeList into an array
 	function makeArray(obj) {
 		if (Array.isArray(obj)) {
 			// use object if already an array
 			return obj;
 		}
+
 		var isArrayLike = typeof obj == 'object' && typeof obj.length == 'number';
 		if (isArrayLike) {
 			// convert nodeList to array
 			return arraySlice.call(obj);
 		}
+
 		// array of single index
 		return [obj];
 	}
+
 	// -------------------------- imagesLoaded -------------------------- //
+
 	/**
 	 * @param {Array, Element, NodeList, String} elem
 	 * @param {Object or Function} options - if function, use as callback
@@ -81,6 +214,7 @@
 			console.error('Bad element for imagesLoaded ' + (queryElem || elem));
 			return;
 		}
+
 		this.elements = makeArray(queryElem);
 		this.options = extend({}, this.options);
 		// shift arguments if no options set
@@ -89,24 +223,33 @@
 		} else {
 			extend(this.options, options);
 		}
+
 		if (onAlways) {
 			this.on('always', onAlways);
 		}
+
 		this.getImages();
+
 		if ($) {
 			// add jQuery Deferred object
 			this.jqDeferred = new $.Deferred();
 		}
+
 		// HACK check async to allow time to bind listeners
 		setTimeout(this.check.bind(this));
 	}
+
 	ImagesLoaded.prototype = Object.create(EvEmitter.prototype);
+
 	ImagesLoaded.prototype.options = {};
+
 	ImagesLoaded.prototype.getImages = function () {
 		this.images = [];
+
 		// filter & find items if we have an item selector
 		this.elements.forEach(this.addElementImages, this);
 	};
+
 	/**
 	 * @param {Node} element
 	 */
@@ -119,6 +262,7 @@
 		if (this.options.background === true) {
 			this.addElementBackgroundImages(elem);
 		}
+
 		// find children
 		// no non-element nodes, #143
 		var nodeType = elem.nodeType;
@@ -131,6 +275,7 @@
 			var img = childImgs[i];
 			this.addImage(img);
 		}
+
 		// get child background images
 		if (typeof this.options.background == 'string') {
 			var children = elem.querySelectorAll(this.options.background);
@@ -140,11 +285,13 @@
 			}
 		}
 	};
+
 	var elementNodeTypes = {
 		1: true,
 		9: true,
 		11: true
 	};
+
 	ImagesLoaded.prototype.addElementBackgroundImages = function (elem) {
 		var style = getComputedStyle(elem);
 		if (!style) {
@@ -162,6 +309,7 @@
 			matches = reURL.exec(style.backgroundImage);
 		}
 	};
+
 	/**
 	 * @param {Image} img
 	 */
@@ -169,10 +317,12 @@
 		var loadingImage = new LoadingImage(img);
 		this.images.push(loadingImage);
 	};
+
 	ImagesLoaded.prototype.addBackground = function (url, elem) {
 		var background = new Background(url, elem);
 		this.images.push(background);
 	};
+
 	ImagesLoaded.prototype.check = function () {
 		var _this = this;
 		this.progressedCount = 0;
@@ -182,17 +332,20 @@
 			this.complete();
 			return;
 		}
+
 		function onProgress(image, elem, message) {
 			// HACK - Chrome triggers event before object properties have changed. #83
 			setTimeout(function () {
 				_this.progress(image, elem, message);
 			});
 		}
+
 		this.images.forEach(function (loadingImage) {
 			loadingImage.once('progress', onProgress);
 			loadingImage.check();
 		});
 	};
+
 	ImagesLoaded.prototype.progress = function (image, elem, message) {
 		this.progressedCount++;
 		this.hasAnyBroken = this.hasAnyBroken || !image.isLoaded;
@@ -205,10 +358,12 @@
 		if (this.progressedCount == this.images.length) {
 			this.complete();
 		}
+
 		if (this.options.debug && console) {
 			console.log('progress: ' + message, image, elem);
 		}
 	};
+
 	ImagesLoaded.prototype.complete = function () {
 		var eventName = this.hasAnyBroken ? 'fail' : 'done';
 		this.isComplete = true;
@@ -219,11 +374,15 @@
 			this.jqDeferred[jqMethod](this);
 		}
 	};
+
 	// --------------------------  -------------------------- //
+
 	function LoadingImage(img) {
 		this.img = img;
 	}
+
 	LoadingImage.prototype = Object.create(EvEmitter.prototype);
+
 	LoadingImage.prototype.check = function () {
 		// If complete is true and browser supports natural sizes,
 		// try to check for image status manually.
@@ -233,6 +392,7 @@
 			this.confirm(this.img.naturalWidth !== 0, 'naturalWidth');
 			return;
 		}
+
 		// If none of the checks above matched, simulate loading on detached element.
 		this.proxyImage = new Image();
 		this.proxyImage.addEventListener('load', this);
@@ -242,16 +402,20 @@
 		this.img.addEventListener('error', this);
 		this.proxyImage.src = this.img.src;
 	};
+
 	LoadingImage.prototype.getIsImageComplete = function () {
 		// check for non-zero, non-undefined naturalWidth
 		// fixes Safari+InfiniteScroll+Masonry bug infinite-scroll#671
 		return this.img.complete && this.img.naturalWidth;
 	};
+
 	LoadingImage.prototype.confirm = function (isLoaded, message) {
 		this.isLoaded = isLoaded;
 		this.emitEvent('progress', [this, this.img, message]);
 	};
+
 	// ----- events ----- //
+
 	// trigger specified handler for event type
 	LoadingImage.prototype.handleEvent = function (event) {
 		var method = 'on' + event.type;
@@ -259,28 +423,35 @@
 			this[method](event);
 		}
 	};
+
 	LoadingImage.prototype.onload = function () {
 		this.confirm(true, 'onload');
 		this.unbindEvents();
 	};
+
 	LoadingImage.prototype.onerror = function () {
 		this.confirm(false, 'onerror');
 		this.unbindEvents();
 	};
+
 	LoadingImage.prototype.unbindEvents = function () {
 		this.proxyImage.removeEventListener('load', this);
 		this.proxyImage.removeEventListener('error', this);
 		this.img.removeEventListener('load', this);
 		this.img.removeEventListener('error', this);
 	};
+
 	// -------------------------- Background -------------------------- //
+
 	function Background(url, element) {
 		this.url = url;
 		this.element = element;
 		this.img = new Image();
 	}
+
 	// inherit LoadingImage prototype
 	Background.prototype = Object.create(LoadingImage.prototype);
+
 	Background.prototype.check = function () {
 		this.img.addEventListener('load', this);
 		this.img.addEventListener('error', this);
@@ -292,15 +463,19 @@
 			this.unbindEvents();
 		}
 	};
+
 	Background.prototype.unbindEvents = function () {
 		this.img.removeEventListener('load', this);
 		this.img.removeEventListener('error', this);
 	};
+
 	Background.prototype.confirm = function (isLoaded, message) {
 		this.isLoaded = isLoaded;
 		this.emitEvent('progress', [this, this.element, message]);
 	};
+
 	// -------------------------- jQuery -------------------------- //
+
 	ImagesLoaded.makeJQueryPlugin = function (jQuery) {
 		jQuery = jQuery || window.jQuery;
 		if (!jQuery) {
@@ -316,6 +491,9 @@
 	};
 	// try making plugin
 	ImagesLoaded.makeJQueryPlugin();
+
 	// --------------------------  -------------------------- //
+
 	return ImagesLoaded;
+
 });
